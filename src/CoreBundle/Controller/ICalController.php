13749a5ef882c2f52290c97925ba197424c1c1a9
pierrick pobelle <pierrick.pobelle@gmail.com>
2018-12-19 16:50:59 +0100

    * ical ajout d'une clé de sécurité

--- src/CoreBundle/Controller/ICalController.php
+++ src/CoreBundle/Controller/ICalController.php
@@ -2,13 +2,19 @@
 
 namespace CoreBundle\Controller;
 
+use BillingBundle\Entity\SalesDocumentDetail;
+use Pkshetlie\SettingsBundle\Controller\ControllerWithSettings;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ICalController extends Controller
+class ICalController extends ControllerWithSettings
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        if($this->getSetting('ical_key', false) && $request->get('key') !== $this->getSetting('ical_key', false)){
+            return new Response('',401);
+        }
         $provider = $this->get('bomo_ical.ics_provider');
 
         $tz = $provider->createTimezone();
@@ -21,7 +27,7 @@ class ICalController extends Controller
         $cal
             ->setName('Micro CRM')
             ->setDescription("Calendrier depuis l'application Micro CRM");
-
+        /** @var SalesDocumentDetail $detail */
         foreach ($this->getDoctrine()->getRepository('BillingBundle:SalesDocumentDetail')
                      ->createQueryBuilder('d')
                      ->where('d.date IS NOT NULL')
@@ -31,8 +37,7 @@ class ICalController extends Controller
                 ->setStartDate($detail->getDate())
                 ->setEndDate($detail->getEndDate())
                 ->setName($detail->getSalesDocument()->getCustomer()->getFullName())
-                ->setDescription($detail->getLabel()."<br/>".$detail->getDescription())
-               ;
+                ->setDescription($detail->getLabel() . "<br/>" . $detail->getDescription() . "<br/>" . $detail->getSalesDocument()->getComment());
         }
         $calStr = $cal->returnCalendar();
 
